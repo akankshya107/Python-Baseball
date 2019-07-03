e2bb88467f02813bc43bc26ded9937daecdfbe30
akankshya107 <akankshyam@gmail.com>
2019-07-03 23:25:02 +0530

    Select play for pitching

--- stats/pitching.py
+++ stats/pitching.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from data import games
 
-plays = games.loc[games['type']=='play']
+plays = games[games['type']=='play']
 
 strike_outs = plays[plays['event'].str.contains('K')]
 strike_outs = strike_outs.groupby(['year', 'game_id']).size()
